ba646da0e2cb376351f3a12000291c3be3b4801a
Pratik Bhaliya <ppmit30@gmail.com>
2020-07-25 17:24:27 +1000

    Loading indicator implemented.

---
+++ Headlines/Headlines/Helpers/ActivityIndicator.swift
@@ -0,0 +1,25 @@
+//
+//  ActivityIndicator.swift
+//  Headlines
+//
+//  Created by Pratik Bhaliya on 25/7/20.
+//  Copyright © 2020 Pratik Bhaliya. All rights reserved.
+//
+
+import UIKit
+
+class SpinnerViewController: UIViewController {
+    var spinner = UIActivityIndicatorView(style: .large)
+
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0)
+
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+        view.addSubview(spinner)
+
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
